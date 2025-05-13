1587f56ebf1e5a8e8a29bb093e38da9e8fbadf3e
KwinZainy <gbadamosiblessing651@gmail.com>
2025-05-13 11:46:36 +0100

    added the js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+// const division = document.getElementById("division")
+// const multiplication = document.getElementById("multiplication")
+// const addition = document.getElementById("addition")
+// const subtraction = document.getElementById("subtraction")
+
+let display = document.getElementById("input")
+let clear = document.getElementById("clear")
+let total = document.getElementById("equalTo")
+
+
+function addNumToDisplay(val){
+    return input.value = input.value + val
+}
+
+
+
+function clearVal(){
+    return input.value = " "
+}
+
+function totalVal(){
+    return input.value = eval(input.value + val)
+}
+
+
+
+
+total.addEventListener("click", totalVal)
+clear.addEventListener("click", clearVal)
 
