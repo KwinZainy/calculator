06e5451cc0e892ce740b85eb914db234530ba70b
KwinZainy <gbadamosiblessing651@gmail.com>
2025-05-17 20:36:52 +0100

    added few js codes

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
-// const division = document.getElementById("division")
-// let display = document.getElementById("input")
-let clear = document.getElementById("clear")
-let total = document.getElementById("equalTo")
-let historyBtn = document.getElementById("historyBtn")
-let history = []
+// // const division = document.getElementById("division")
+// // let display = document.getElementById("input")
+// let clear = document.getElementById("clear")
+// let total = document.getElementById("equalTo")
+// let historyBtn = document.getElementById("historyBtn")
+// let history = []
 
-function addNumToDisplay(val){
-    return input.value = input.value + val
-}
-
-
-
-function clearVal(){
-    return input.value = ""
-}
-
-function totalVal(){
-    history.push (input.value)
-    return input.value = eval(input.value)
-}
-
-historyBtn.addEventListeners( "click",function(){
-    value.input 
-})
+// function addNumToDisplay(val){
+//     return input.value = input.value + val
+// }
 
 
-total.addEventListener("click", totalVal)
-clear.addEventListener("click", clearVal)
 
+// function clearVal(){
+//     return input.value = ""
+// }
+
+// function totalVal(){
+//     history.push (input.value)
+//     return input.value = eval(input.value)
+// }
+
+
+// function reload(){
+//       input.value = history.push
+//     alert("zainab")
+
+// }
+
+// total.addEventListener("click", totalVal)
+// clear.addEventListener("click", clearVal)
+// historyBtn.addEventListeners( "click", reload)
+  
